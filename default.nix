1760728d6d2e4c8c954389cca32fefac8a3f5549
makefu <github@syntax-fehler.de>
2018-08-08 19:47:43 +0200

    package.nix -> default.nix

--- default.nix
+++ default.nix
@@ -1,46 +1,15 @@
-{ pkgs, ... }:
-with import <stockholm/lib>;
-
-let
-  shack-announce =  pkgs.callPackage ./package.nix {};
-  home = "/var/lib/shackannounce";
-  user = "shackannounce";
-  creds = (toString <secrets>) + "/shack-announce.json";
-in
-{
-  users.users.${user}= {
-    uid = genid user;
-    inherit home;
-    createHome = true;
-  };
-  systemd.services.shack-announce = {
-    description = "Announce shack events";
-    startAt = "*:0/30";
-    path = [ shack-announce ];
-    serviceConfig  = {
-      WorkingDirectory = home;
-      User = user;
-      PermissionsStartOnly = true;
-      ExecStartPre = pkgs.writeDash "shack-announce-pre" ''
-        set -eu
-        cp ${creds} creds.json
-        chown ${user} creds.json
-      '';
-      ExecStart = pkgs.writeDash "shack-announce" ''
-        if test ! -e announce.state; then
-          echo "initializing state"
-          announce-daemon \
-            --lol INFO \
-            --creds creds.json \
-            --state announce.state \
-            --clean --init
-        fi
-        echo "Running announce"
-        announce-daemon \
-           --lol INFO \
-           --creds creds.json \
-           --state announce.state
-      '';
-    };
-  };
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.python3Packages.buildPythonPackage rec {
+  pname = "shack_announce";
+  version = "0.0.5";
+  propagatedBuildInputs = with pkgs.python3Packages; [
+    ( callPackage ./mastodon.nix {} )
+    requests twitter docopt dateutil pytz praw
+    ( callPackage ./black.nix {} )
+    ( callPackage ./facebook.nix {} )
+  ];
+  src = ./.;
+  #shellHook =''
+  #  HISTFILE="${toString ./.}/.histfile"
+  #'' ;
 }
